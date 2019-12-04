c9e4d8f83139e14a8add448e9e1bd170934934aa
Asher Abrams <asher813@aol.com>
2019-12-03 20:03:00 -0800

    Update main.c
    Using q (instead of m) for 1/r, setting q=10, and limit down to 20.  Output illustrates decimal expansion of 1/9 = 1/(10-1) as a power series.

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int main(int argc, char **argv)
 {
 	printf("hello R-series\n");
 int n;
-float m;
-	m = 3;
-float r = (1/m);
+float q;
+	q = 10;
+float r = (1/q);
 float rTerm =1 ;
 float a = 1.0;
 float x;
-int limit = 100;
+int limit = 20;
 
-// The sum will quickly approach 1 + 1/(m-1).
+// The sum will quickly approach 1 + 1/(q-1).
 
 x = a * rTerm;
 for(n = 1; n <= limit; n++)
